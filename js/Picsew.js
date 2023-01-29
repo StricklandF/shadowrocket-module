e57b02caf4787426f528181565dbe26c8819069d
itcast-l <45306550+itcast-l@users.noreply.github.com>
2023-01-29 15:45:20 +0800

    Update Picsew.js

--- js/Picsew.js
+++ js/Picsew.js
@@ -9,7 +9,7 @@
 
 [rewrite_local]
 # > Picsew解锁专业版
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/PicsewProCrack.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://github.com/itcast-l/shadowrocket-module/blob/main/js/Picsew.js
 
 [mitm] 
 hostname = buy.itunes.apple.com
